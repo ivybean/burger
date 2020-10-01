de11e4e4dd0ebf838534b839a5d00c00df7cc852
Ivy Bean <ivieking@gmail.com>
2020-10-01 15:38:17 -0400

    initial commit (created schema.sql and seeds.sql)

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('American Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Carolina Burger', true);
+
