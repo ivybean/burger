f2e934cc85d26fabad4eb047f718354040a9d66c
Ivy Bean <ivieking@gmail.com>
2020-10-01 15:54:22 -0400

    config folder and connection.js/orm.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('American Burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Carolina Burger', true);
-
