79e198adf2f0ef70472369fd59e5f96a929966b0
Ivy Bean <ivieking@gmail.com>
2020-10-27 18:19:42 -0400

    added JawsDB to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  user: "root",
-  password: "12345678",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWS_URL) {
+  connection = mysql.createConnection(process.env.JAWS_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "12345678",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
