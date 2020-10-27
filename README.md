6a31ca6a7eed4399bea7904265b0e4451d707ec3
Ivy Bean <ivieking@gmail.com>
2020-10-27 17:47:11 -0400

    styling changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# burger
+# Burger
+An app that allows users to input a the name of a burger which will then be displayed in a list. From the list of bugers users can "devour" a burger and it will be added to the "Devoured Burgers" list.  
+
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,50 +1,45 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
 $(function () {
   $(".delete-burger").on("click", function (event) {
     const id = $(this).data("id");
 
     $.ajax("api/burgers/" + id, {
       type: "DELETE",
-    }).then(function() {
+    }).then(function () {
       console.log("Deleted Burger");
       location.reload();
     })
   })
 
-    $(".add-burger").on("submit", function (event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
+  $(".add-burger-btn").on("submit", function (event) {
+    event.preventDefault();
 
-      const newBurger = {
-        burger_name: $(".burger-input").val().trim(),
-        devoured: 0
-      };
+    const newBurger = {
+      burger_name: $(".burger-input").val().trim(),
+      devoured: 0,
+    };
 
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(function () {
-          console.log("added new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-
-    $(".devour-burger").on("click", function (event) {
-      const id = $(this).data("id");
-
-      // Send the PUT request.
-      $.ajax("/api/burger/" + id, {
-        type: "PUT",
-        data: { devoured: 1 }
-      }).then(function () {
-          console.log("devoured burger ", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("added new burger");
+      location.reload();
+    }
+    );
   });
+
+  $(".devour-burger-btn").on("click", function (event) {
+    const id = $(this).data("id");
+
+    $.ajax("/api/burger/" + id, {
+      type: "PUT",
+      data: { devoured: 1 },
+    }).then(function () {
+      console.log("devoured burger ", id);
+      location.reload();
+    }
+    );
+  });
+});
 
